4f57e575bc40225469a3c73e3a7e0a80d81dee0a
oxaji <oxaji0306@yahoo.co.jp>
2020-07-05 00:31:51 +0900

    ランキング機能
    ランキング機能実装
    問題点：いいね一覧が表示されない

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "toppage#index"
+  resources :rankings, only: [:index]
   resources :charts, only: [:index, :new, :create, :show] do
     resources :likes, only: [:create, :destroy]
   end
