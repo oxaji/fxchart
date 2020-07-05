8fb3caaa23244e033ee6528ac8de262bb01141d5
oxaji <oxaji0306@yahoo.co.jp>
2020-07-06 01:35:38 +0900

    プロフィール
    プロフィールページ作成

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -2,7 +2,7 @@ class ToppageController < ApplicationController
 
   def index
     @chart = Chart.find_by(id: params[:id])
-    @charts = Chart.all.page(params[:page]).per(6)
+    @charts = Chart.all.order(created_at: :desc).page(params[:page]).per(6)
     @likes_count = Like.where(chart_id: @chart).count
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "toppage#index"
   resources :rankings, only: [:index]
+  resources :profiles, only: [:index]
   resources :charts, only: [:index, :new, :create, :show] do
     resources :likes, only: [:create, :destroy]
   end
